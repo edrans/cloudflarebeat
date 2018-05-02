1675c022d26ea5c5b0f188237459eaa79412f7ee
Jacob Verhoeks <jjverhoeks@edrans.com>
2018-05-02 11:39:05 +0200

    Adding Retry loop for 429 api limit

--- cloudflare/client.go
+++ cloudflare/client.go
@@ -2,6 +2,7 @@ package cloudflare
 
 import (
 	//"bufio"
+	"errors"
 	"fmt"
 	"net/url"
 	"time"
@@ -25,6 +26,7 @@ type CloudflareClient struct {
 	RequestLogFile *RequestLogFile
 	LogfileName    string
 	uri            string
+	StatePath      string
 	debug          bool
 }
 
@@ -46,6 +48,9 @@ func NewClient(params map[string]interface{}) *CloudflareClient {
 		c.debug = params["debug"].(bool)
 	}
 
+	if _, ok := params["state_path"]; ok {
+		c.StatePath = params["state_path"].(string)
+	}
 	return c
 }
 
@@ -64,10 +69,14 @@ func (c *CloudflareClient) doRequest(params map[string]interface{}) (string, err
 		qsa.Set("count", fmt.Sprintf("%d", params["count"].(int)))
 	}
 
+	// goreq.DefaultTransport = &http.Transport{Dial: goreq.DefaultDialer.Dial,
+	// 	Proxy:              http.ProxyFromEnvironment,
+	// 	DisableCompression: true}
+
 	req := goreq.Request{
 		Uri:         apiURL,
 		Timeout:     10 * time.Minute,
-		ShowDebug:   c.debug,
+		ShowDebug:   c.debug, // true
 		QueryString: qsa,
 	}
 
@@ -79,29 +88,50 @@ func (c *CloudflareClient) doRequest(params map[string]interface{}) (string, err
 		req.AddHeader("X-Auth-Email", c.Email)
 	}
 
-	logp.Debug("http", "Downloading log file...")
+	logp.Debug("http", "%d Downloading log file...", params["time_start"])
 
-	response, err := req.Do()
-	if err != nil {
-		return "", err
+	for i := 0; i < 4; i++ {
+		response, err := req.Do()
+		if err != nil {
+			logp.Debug("http", "%d Error request  %s", err)
+			return "", err
+		}
+
+		logp.Debug("http", "%d Try %d Request code %d, %s %s", params["time_start"], i, response.StatusCode, response.Status, qsa)
+		logp.Debug("http", "%d Try %d Request Content type %s", params["time_start"], i, response.Header.Get("Content-Type"))
+
+		if (response.StatusCode == 200) && (response.Header.Get("Content-Type") == "application/json") {
+			// Now need to save all the resposne content to a file
+			logFileName := fmt.Sprintf("cloudflare_logs_%d_to_%d.txt.gz", params["time_start"].(int), params["time_end"].(int))
+			rlf := NewRequestLogFile(logFileName, c.StatePath)
+
+			//	logp.Debug("http", "Body"+nBody)
+			nBytes, err := rlf.SaveFromHttpResponseBody(response.Body)
+			if err != nil {
+				logp.Debug("http", "%d Error saving %s", params["time_start"], err)
+				return "", err
+			}
+			logp.Debug("http", "%d Downloaded %d bytes to %s %s", params["time_start"], nBytes, logFileName, c.StatePath)
+			if nBytes <= 23 {
+				logp.Debug("http", "%d Error body %s", params["time_start"], response.Status)
+				return "", errors.New("Request body is empty")
+			}
+			return logFileName, nil
+		} else {
+			logp.Debug("http", "%d Wrong status code : %s", params["time_start"], response.Status)
+		}
+		// sleep a second
+		logp.Debug("http", "%d Sleeping 2 seconds", params["time_start"])
+		time.Sleep(2 * time.Second)
 	}
-
-	// Now need to save all the resposne content to a file
-	logFileName := fmt.Sprintf("cloudflare_logs_%d_to_%d.txt.gz", params["time_start"].(int), params["time_end"].(int))
-	rlf := NewRequestLogFile(logFileName)
-	nBytes, err := rlf.SaveFromHttpResponseBody(response.Body)
-	if err != nil {
-		return "", err
-	}
-
-	logp.Debug("http", "Downloaded %d bytes", nBytes)
-
-	return logFileName, nil
+	logp.Debug("http", "%d Returning empty", params["time_start"])
+	return "", nil
 }
 
 func (c *CloudflareClient) GetLogRangeFromTimestamp(opts map[string]interface{}) (string, error) {
 	filename, err := c.doRequest(opts)
 	if err != nil {
+		logp.Debug("http", "Error doing request %s", err)
 		return "", err
 	}
 	return filename, nil
